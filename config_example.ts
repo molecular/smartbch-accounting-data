ee7f257667241817aee1be8adafb43ab598aa88c
molecular <molec@posteo.de>
2022-05-03 07:40:06 +0200

    re-add config_example.ts

---
+++ config_example.ts
@@ -0,0 +1,28 @@
+export const config = {
+
+	// the smartbch node rpc interface to use for pulling data
+	"api": {
+		"apiType": "web3",
+		"apiVersion": "v1",
+		"network": "Mainnet",
+
+		//"apiEndpoint": "http://nil.criptolayer.net:8545"
+		"apiEndpoint": "https://smartbch.fountainhead.cash/mainnet:8545"
+		// "apiEndpoint": "https://smartbch.fountainhead.cash/mainnet"
+		// "apiEndpoint": "https://global.uat.cash"
+		// "apiEndpoint": "https://rpc.uatvo.com"
+		// "apiEndpoint": "https://moeing.tech:9545"
+	},	
+
+	// list the smartbch account adresses you want to export data for
+	"my_addresses": [ 
+		// "0xabc123...", // more accounts
+		// "0xdef456...", // more accounts
+	],
+
+	// output configuration
+	"output": { 
+		"decimals": 18,
+		"separate_file_per_contract": false,
+	}
+}
